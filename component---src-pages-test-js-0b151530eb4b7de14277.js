3393d84e3dca6fe900d1f21a544fb383b5f7bbe7
rubengerritsen <r.h.j.gerritsen@gmail.com>
2020-11-15 18:37:56 +0100

    Updates

---
+++ component---src-pages-test-js-0b151530eb4b7de14277.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{h5Py:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n("qKvR");t.default=function(){var e=Object(a.useState)(""),t=e[0],n=e[1],r=Object(a.useState)(""),c=r[0],o=r[1];return Object(u.b)("div",null,Object(u.b)("input",{name:"email",value:t,onChange:function(e){return n(e.target.value)}}),Object(u.b)("input",{type:"password",name:"password",value:c,onChange:function(e){return o(e.target.value)}}))}}}]);
+//# sourceMappingURL=component---src-pages-test-js-0b151530eb4b7de14277.js.map
